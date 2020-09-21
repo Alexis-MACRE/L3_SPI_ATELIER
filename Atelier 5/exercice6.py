44df56097428e4138d7615ce4a98359e2d5ce65b
Thunder-web <59866108+Thunder-web@users.noreply.github.com>
2020-09-21 15:44:33 +0200

    fonction de test

--- Atelier 5/exercice6.py
+++ Atelier 5/exercice6.py
@@ -7,6 +7,7 @@ from random import *
 import matplotlib.pyplot as plt
 import numpy as np
 import time
+from exercice5 import perf_mix
 
 from exercice1 import gen_list_random_int
 
@@ -30,49 +31,15 @@ def sort_list(liste_a_trier:list)->list:
      
      return liste_croissant
 
-def perf_mix(fonction1:callable,fonction2:callable,liste_tailles:list,nbr_execs:int)->tuple:
-     """Fonction de mesure de comparaison durée d'execution entre deux fonctions.
-     Calcule la moyenne de temps d'execution pour chacune des tailles et en fait une liste (liste_moyennes)
 
-     Arguments:
-     fonction1      -- la première fonction dont on souhaite mesurer le temps d'execution
-     fonction2      -- la seconde fonction
-     liste_tailles  -- liste contanant les tailles des list[int] avec lesquelles on souhaite tester les fonctions
-     nbr_execs      -- nombre d'executions pour calculer la moyenne
-     
-     Retourne le couple (resultat_fonction1,resultat_fonction2) 
-     avec resultat_fonction sous la forme (liste_tailles,liste_moyennes)
-     """
-     
-     fonctions = [fonction1,fonction2]
-     retour_final = [None,None]     
-     numero_fonction = 0
-
-     for fonction in fonctions:
-          liste_moyennes = []
-          somme_temps = 0
-          for taille in liste_tailles:
-               for i in range(1,nbr_execs):
-                    temps_debut = time.perf_counter()
-                    fonction(gen_list_random_int(taille))
-                    temps_fin = time.perf_counter()
-
-                    temps_ecoule = temps_fin-temps_debut
-                    somme_temps+=temps_ecoule
-               liste_moyennes += [somme_temps/nbr_execs]
-               
-          retour_final[numero_fonction]=(liste_tailles,liste_moyennes)
-          numero_fonction+=1
-
-     return (retour_final[0],retour_final[1])
 
 resultats = perf_mix(sort_list,sorted,[100,500,1000],100)
 
 fig, ax = plt.subplots()
 
-ax.plot(resultats[0][0],resultats[0][1],label="extract_elements_list")
+ax.plot(resultats[0][0],resultats[0][1],label="sort_list")
 
-ax.plot(resultats[1][0],resultats[1][1],label="sample")
+ax.plot(resultats[1][0],resultats[1][1],label="sorted")
 
 ax.legend(loc="upper center",shadow=True, fontsize='x-large')
 
--- Atelier 5/fonctions_persos.py
+++ Atelier 5/fonctions_persos.py
@@ -1,13 +1,12 @@
 # Auteur(s) : MACRE Alexis
 # Date : 21/09/2020
-# Version : 3.0
-# Description : Fonction pour tester une fonction donnée
+# Version : 4.0
+# Description : Fonction de test
 
 
-def test_fonction(fonction:callable,valeurs:list,valeurs_attendues:list,nb_param:int=1,affichage_variable:bool=True) -> None :
+def test_fonction(fonction:callable,valeurs:list,valeurs_attendues:list,affichage_variable:bool=True) -> None :
      """Fonction qui permet de tester une fonction pour chacune des valeurs passées en paramètre.
      Elle compare les valeurs que la fonction retourne à celles qui sont attendues, puis affiche les résultats du test.
-     Obligation de préciser le nombre de paramètres de la fonction testée quand ils sont >=2.
      Possibilité de désactiver l'affichage des valeurs d'entrées, dans le cas par exemple où cette dernière est très 
      volumineuse (comme une liste de 15000 mots)
      
@@ -15,39 +14,23 @@ def test_fonction(fonction:callable,valeurs:list,valeurs_attendues:list,nb_param
      fonction            -- la fonction à tester
      valeurs             -- liste des paramètres en entrée pour chacun des tests
      valeurs_attendues   -- liste des valeurs de retour attendues
-     nb_param            -- nombre de paramètres de la fonction, par défaut = 1
      affichage           -- affichage ou non de la valeur en entrée
      """
 
      for i in range(len(valeurs)):                                              
 
-          if nb_param == 1:
-               resultat = fonction(valeurs[i]) 
+          resultat = fonction(*valeurs[i]) 
 
-
-          elif nb_param == 2:
-               
-               resultat = fonction(valeurs[i][0],valeurs[i][1])
-
-
-          elif nb_param == 3:
-               resultat = fonction(valeurs[i][0],valeurs[i][1],valeurs[i][2])
-
-
-          elif nb_param == 4:
-               resultat = fonction(valeurs[i][0],valeurs[i][1],valeurs[i][2],valeurs[i][3])
-
+          if affichage_variable:
+               val_affiche = "pour " + str(valeurs[i])
+          else:
+               val_affiche = "numero " + str(i)
 
           if resultat!=valeurs_attendues[i]:
-               if affichage_variable:
-                    print("Test pour",valeurs[i],": Resultat Incorrect",resultat,"au lieu de",valeurs_attendues[i])
-               else:
-                    print("Test numero",i,": Resultat Incorrect",resultat,"au lieu de",valeurs_attendues[i])
+               print("Test",val_affiche,": Resultat Incorrect",resultat,"au lieu de",valeurs_attendues[i])
           else:
-               if affichage_variable:
-                    print("Test pour",valeurs[i],": Resultat Correct",resultat)
-               else:
-                    print("Test numero",i,": Resultat Correct",resultat) 
+               print("Test",val_affiche,": Resultat Correct",resultat)
+
 
 
 
